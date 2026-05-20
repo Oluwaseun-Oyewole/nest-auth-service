d1b431b81f6cc1f76c600437a09a4b7b262d0d2d
Oluwaseun Oyewole <findseunoyewole@gmail.com>
2026-05-20 21:44:37 +0100

    feat: clean up auth services

--- src/migrations/1779308045111-migrations.ts
+++
@@ -1,7 +0,0 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
-
-export class Migrations1779308045111 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
-
-  public async down(queryRunner: QueryRunner): Promise<void> {}
-}
